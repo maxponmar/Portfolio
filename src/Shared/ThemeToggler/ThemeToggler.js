3152872d2bad50645c23656027dd80375807b78f
Maximiliano Ponce Marquez <maxponce-marquez@outlook.com>
2021-04-08 07:11:23 -0700

    Theme Toggler implemented, scss variables replaced with css variables

--- src/Shared/ThemeToggler/ThemeToggler.js
+++ src/Shared/ThemeToggler/ThemeToggler.js
@@ -1,16 +1,53 @@
-import './ThemeToggler.scss';
+import "./ThemeToggler.scss";
 
-import sun from '../../Assets/SVG/sunny.svg';
-import moon from '../../Assets/SVG/half-moon.svg';
+import { useEffect } from "react";
 
+import sun from "../../Assets/SVG/sunny.svg";
+import moon from "../../Assets/SVG/half-moon.svg";
+
+const root = document.querySelector(":root");
+let sunIcon = document.querySelector(".sun");
+let moonIcon = document.querySelector(".moon");
+
+const setDarkTheme = function () {
+  root.style.setProperty("--color-primary", "#798777");
+  root.style.setProperty("--color-white", "#010101");
+  root.style.setProperty("--color-black", "#bbe1fa");
+  root.style.setProperty("--color-grey-light", "#222");
+  root.style.setProperty("--color-icons", "#010101");
+  moonIcon.classList.remove("hidden");
+  sunIcon.classList.add("hidden");
+};
+
+const setLightTheme = function () {
+  root.style.setProperty("--color-primary", "#19456b");
+  root.style.setProperty("--color-white", "#eee");
+  root.style.setProperty("--color-black", "#23120b");
+  root.style.setProperty("--color-grey-light", "#e2e2e2");
+  root.style.setProperty("--color-icons", "#eee");
+  moonIcon.classList.add("hidden");
+  sunIcon.classList.remove("hidden");
+};
 
 function ThemeToggler() {
+  useEffect(() => {
+    sunIcon = document.querySelector(".sun");
+    moonIcon = document.querySelector(".moon");
+  });
   return (
-    <div className='theme-toggler'>
-      <img className='theme-toggler__icon' src={sun}/>
-      <img className='theme-toggler__icon hidden' src={moon}/>
+    <div className="theme-toggler">
+      <img
+        className="theme-toggler__icon sun "
+        src={sun}
+        onClick={setDarkTheme}
+      />
+      <img
+        className="theme-toggler__icon moon hidden"
+        src={moon}
+        onClick={setLightTheme}
+      />
     </div>
-  )
+  );
 }
 
 export default ThemeToggler;
---
+++ src/Shared/ThemeToggler/themes.js
@@ -0,0 +1,11 @@
+const lightTheme = {
+  bodyColor = '#eeeeee',
+  fontColorPrincipal = '#719fb0',
+  fontColorSecondary = '#23120b'
+}
+
+const darkTheme = {
+  bodyColor = '#222831',
+  fontColorPrincipal = '#ececec',
+  fontColorSecondary = '#bbe1fa'
+}
