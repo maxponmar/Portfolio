07029902d169ff448c1849df83531cb3cc3ebf96
Maximiliano Ponce Marquez <maxponce-marquez@outlook.com>
2021-04-10 09:09:59 -0700

    Link from home card replaced with a button

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -1,11 +1,16 @@
-import './Button.scss';
+import "./Button.scss";
 
 const Button = ({ title, link }) => {
   return (
-    <div className="button">
-      <a href={link}>{title}</a>
+    <div
+      className="button"
+      onClick={() => {
+        document.querySelector(link).scrollIntoView();
+      }}
+    >
+      <button>{title}</button>
     </div>
-  )
-}
+  );
+};
 
 export default Button;
